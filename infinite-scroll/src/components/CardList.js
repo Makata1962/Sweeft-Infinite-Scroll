b669691eae8a0b158b80b8b7cc958af80a1f867c
Ilia Pachulia <ipachulia@evolution.com>
2023-03-17 20:00:52 +0400

    cards were styled

--- infinite-scroll/src/components/CardList.js
+++ infinite-scroll/src/components/CardList.js
@@ -1,27 +1,52 @@
-import React from "react";
-import Card from "./Card";
+import React, { useState, useRef, useCallback } from "react";
 import "./CardList.style.css";
+import useFetch from "./hooks/useFetch";
 
-const CardList = React.forwardRef(({ cards }, cardRef) => {
-  console.log(cards);
+const CardList = () => {
+  const [pageNumber, setPageNumber] = useState(1);
+  const { cards, hasMore, loading, error } = useFetch(pageNumber);
+
+  const observer = useRef();
+  const lastCardElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevNum) => prevNum + 1);
+        }
+      });
+
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
+
   return (
     <div className="card_list">
       {cards.map((card, index) => {
         if (cards.length === index + 1) {
           return (
-            <Card
-              ref={cardRef}
-              key={index}
-              image={card.imageUrl}
-              name={card.title}
-            />
+            <div ref={lastCardElementRef} className="card" key={index}>
+              <img src={card.imageUrl} alt={card.title} />
+              <h3>{card.title}</h3>
+              <p>{card.description}</p>
+            </div>
           );
         } else {
-          return <Card key={index} image={card.imageUrl} name={card.title} />;
+          return (
+            <div className="card" key={index}>
+              <img src={card.imageUrl} alt={card.title} />
+              <h3>{card.title}</h3>
+              <p>{card.description}</p>
+            </div>
+          );
         }
       })}
+      {loading && <div>Loading...</div>}
+      {error && <div>Error...</div>}
     </div>
   );
-});
+};
 
 export default CardList;
